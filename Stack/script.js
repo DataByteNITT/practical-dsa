3284f908bdaaaa429843b5479fac58286cdc567d
vij.duggirala <viji9810@gmail.com>
2020-03-31 02:34:12 +0530

    hover feature remaining

--- Stack/script.js
+++ Stack/script.js
@@ -18,25 +18,92 @@ class stack{
 
     }
 
-    push(imgData){
+    gettop(){
+        return this.top;
+    }
+
+    getData(i){
+        i=15-i;
+        k= s1.size();
+        i=k-i;
+        j=1;
+        
+        var temp= s1.gettop();
+        while(j<i)
+          {  temp = temp.next; j++ } 
+        console.log(temp.data);
+        return temp.data;
+
+
+    }
+
+    updatePushDisplay(){
+        k = this.size();
+        i=15-k;
+
+        let c = document.getElementById(i);
+        
+        c.style.backgroundColor="red";
+        
+
+        c.addEventListener('mouseover' , function(e){
+            imgData = s1.getData(e.target.c);
+            currimgData = ctx.getImageData(0 ,0 , canvas.width , canvas.height);
+
+            ctx.putImageData(imgData , 0 , 0);
+            setTimeout(function() {
+                ctx.putImageData(currimgData, 0 ,0);
+              }, 400);
+        }, false);
+    
+
+    }
+
+
+
+    updatePopDisplay(){
+        k = this.size();
+        i=15-k;
+
+        let c = document.getElementById(i);
+        
+            c.style.backgroundColor="white";
+    }
+
+    
+
+
+    push(imgData , is_undo){
         var newNode = new Node();
         newNode.next = this.top;
         newNode.data = imgData;
         this.top = newNode;
+
+        if(is_undo)
+        this.updatePushDisplay();
+
+       
+        console.log(this.size());
     }
 
-    pop(){
+    pop(is_undo){
         if(this.top == null)
             return null;
         
+        if(is_undo)
+        this.updatePopDisplay();
+        
         this.top = this.top.next;
+        console.log(this.size());
+        
+       
     }
 
 
     Top(){
         if(this.top == null)
             return null;
-        console.log(this.top.data);
+       
         return this.top.data;
     }
 
@@ -49,7 +116,7 @@ class stack{
     size(){
         let i =0;
         var temp = new Node();
-        temp = this.top();
+        temp = this.top;
         while(temp!==null)
         {
             temp = temp.next;
@@ -57,45 +124,74 @@ class stack{
         }
         return i;
     }
+
+    
+
+
+
 };
 
 
 
 
 //variables
-var prevX , prevY , currX , currY, flag, url, save_flag, imgData;
+var j ,k ,prevX , prevY , currX , currY, flag, url, save_flag, imgData , currimgData;
 save_flag=true;
 flag = false;
 let s1 = new stack();
 let s2 = new stack();
+var i=0;
+var dot_flag=true;
+
+
+let cell = document.getElementsByTagName("td");
+cell[0].setAttribute('id' , '1');
+let row = document.getElementById("row");
+let table = document.getElementById("stack");
+
+
+for(i=1;i<=13;i++){
+
+    let tr= document.createElement("tr");
+    let new_cell  = cell[0].cloneNode(true);
+    new_cell.setAttribute('id' , i+1);
+    tr.appendChild(new_cell);
+    table.appendChild(tr);
+}
+
+
+
+
+
+
 
 
 //functions for the 3 buttons
 document.getElementById("undo").onclick = function(){
       
     imgData = ctx.getImageData(0 ,0 , canvas.width , canvas.height);
-    s2.push(imgData);
+    s2.push(imgData , 0);
 
     imgData = s1.Top();
     if(imgData!==null){
-    s1.pop();
-    s2.push(imgData);
+    s1.pop(1);
+    s2.push(imgData , 0);
     ctx.putImageData(imgData, 0, 0);
     }
 }
 document.getElementById("redo").onclick = function(){
     imgData = s2.Top();
     if(imgData !== null){
-    s1.push(imgData);
-    s2.pop();
+    s1.push(imgData , 1);
+    s2.pop(0);
     }
 
 
-    console.log(imgData);
+  //  console.log(imgData);
     imgData = s2.Top();
     if(imgData!==null){
     ctx.putImageData(imgData , 0 , 0);
-    s2.pop();
+    s2.pop(0);
     }
 
 }
@@ -136,19 +232,31 @@ function find( state ,  e){
         prevY = currY;
         currX = e.clientX - canvas.offsetLeft;
         currY = e.clientY - canvas.offsetTop;
-        
+        flag  = true;
         //redo stack is emptied, when mouse is pressed. 
         s2.deleteall();
+         
         if(save_flag){
             imgData = ctx.getImageData(0, 0 , canvas.width , canvas.height);
-            console.log(imgData);
-            s1.push(imgData);
+        //    console.log(imgData);
+            s1.push(imgData  , 1);
+            save_flag=false;
         }
-        flag  = true;
+        if(dot_flag)
+        {
+            dot_flag=false;
+            ctx.beginPath();
+            ctx.fillStyle="white";
+            ctx.fillRect(currX, currY, 7, 7);
+            ctx.closePath();
+           
+        }
+       
+     
     }
 
     if(state== 'up' || state == 'out')
-    {    flag=false; save_flag=true; }
+    {    flag=false; save_flag=true;  dot_flag=true; }
     
     if(state == 'move' && flag){
         prevX = currX;
@@ -175,5 +283,17 @@ function Start(){
     //s2 to store the redo states
     s2.deleteall();
 }
+
+
+function show(){
+
+}
+
+
+
+
+
+
+
 
 Start();
